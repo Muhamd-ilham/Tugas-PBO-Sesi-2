e275f2d049f70de86759c6f834481937a2d1e801
𝚆𝚘𝚠. 𝙲𝚘𝚖 <160624200+Muhamd-ilham@users.noreply.github.com>
2026-04-20 14:15:35 +0700

    Create Komputer.java

---
+++ Komputer.java
@@ -0,0 +1,26 @@
+public class Komputer {
+    String jenis_komputer;
+    private String merk;
+
+    public void setDataKomputer(String jenis, String merk) {
+        jenis_komputer = jenis;
+        this.merk = merk;
+    }
+
+    public String getJenis() {
+        return jenis_komputer;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public static void main(String[] args) {
+        Komputer mykom = new Komputer();
+        
+        mykom.setDataKomputer("LAPTOP", "MACBOOK");
+        
+        System.out.println("Jenis Komputer : " + mykom.getJenis());
+        System.out.println("Merk Komputer  : " + mykom.getMerk());
+    }
+}
